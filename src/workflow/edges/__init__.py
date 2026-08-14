416e49f635497b9a4fad8775ea3e069a3b19a9c1
Lorraine Frontelli <lorrainefrontelli@gmail.com>
2026-08-14 17:39:06 -0300

    feat: add decide_post_judge edge

--- src/workflow/edges/__init__.py
+++ src/workflow/edges/__init__.py
@@ -1,2 +1,3 @@
 from .routing_edges import decide_post_input_guardrail as decide_post_input_guardrail
+from .routing_edges import decide_post_judge as decide_post_judge
 from .routing_edges import decide_post_router as decide_post_router
