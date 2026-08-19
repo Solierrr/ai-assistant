ed735b1786d41568ed8976d75a28cbd536af8051
Lorraine Frontelli <lorrainefrontelli@gmail.com>
2026-08-19 17:00:21 -0300

    feat: pluga faq_retriever no node do agente faq_reader

--- src/workflow/nodes/faq_reader_node.py
+++ src/workflow/nodes/faq_reader_node.py
@@ -2,13 +2,14 @@ from langchain_core.messages import AIMessage
 
 from src.agents.base.base_agent import build_agent
 from src.agents.specialist.faq_reader.faq_reader_prompt import FAQ_READER_AGENT
+from src.agents.specialist.faq_reader.tools.faq_retriever import faq_retriever
 from src.workflow.nodes.context import messages_with_summary
 from src.workflow.state import GraphState
 from src.workflow.turn_tracking import append_turn_agent
 
 
 def faq_reader_node(state: GraphState) -> dict:
-    agent = build_agent(FAQ_READER_AGENT, tools=[])
+    agent = build_agent(FAQ_READER_AGENT, tools=[faq_retriever])
     result = agent.invoke({"messages": messages_with_summary(state)})
     last_message = result["messages"][-1]
 
